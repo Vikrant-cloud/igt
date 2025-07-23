be618172cb62c74ed18742e076d49222eec748b9
Vikrant Sharma <vikrantsharma0959@gmail.com>
2025-07-24 03:06:26 +0530

    log message

--- src/routes/webhook.js
+++ src/routes/webhook.js
@@ -8,7 +8,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 router.post('/', async (req, res) => {
     const sig = req.headers['stripe-signature'];
     let event;
-    console.log("Received webhook event:", req.body, sig);
+    console.log("Received webhook event:", req.body, sig, process.env.STRIPE_WEBHOOK_SECRET);
 
 
     try {
