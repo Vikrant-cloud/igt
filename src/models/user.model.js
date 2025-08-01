c1c85abd63519b7050487985a186ae68c7c2643a
Vikrant Sharma <vikrantsharma0959@gmail.com>
2025-08-01 21:10:17 +0530

    remove duplicate index

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -9,7 +9,7 @@ const userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
-        unique: true, // Unique index
+        // unique: true, // Unique index
         match: /.+\@.+\..+/,
     },
     password: {
