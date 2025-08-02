e89efa8dd998be2c47800d94a1bcc267bc1611ce
Vikrant Sharma <vikrantsharma0959@gmail.com>
2025-08-02 16:49:38 +0530

    active sidebar options

--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -1,28 +1,30 @@
-import { Link } from 'react-router';
+import { Link, useLocation } from 'react-router';
 import { useAuth } from '@/hooks/useAuth';
 import { HiOutlineHome, HiOutlineUsers, HiOutlineDocumentText, HiOutlineCreditCard } from 'react-icons/hi';
 
 export default function SideBar() {
   const { user } = useAuth();
+  const location = useLocation();
+  const isActive = (path: string) => location.pathname === path ? 'text-indigo-600 bg-indigo-100 flex items-center gap-3 hover:text-indigo-600 hover:bg-indigo-100 rounded-lg px-2 py-2 transition-colors' : 'flex items-center gap-3 hover:text-indigo-600 hover:bg-indigo-100 rounded-lg px-2 py-2 transition-colors';
   return (
     <aside className="   md:flex flex-col py-8 space-y-6">
       <h2 className="text-xl font-bold text-left text-indigo-600">Quick links</h2>
       <nav className="flex flex-col space-y-4 text-gray-700 font-medium">
-        <Link to="/" className="flex items-center gap-3 hover:text-indigo-600 hover:bg-indigo-100 rounded-lg px-2 py-2 transition-colors">
+        <Link to="/" className={isActive('/')}>
           <HiOutlineHome size={20} />
           Home
         </Link>
         {user?.role === 'admin' && (
-          <Link to={`/users`} className="flex items-center gap-3 hover:text-indigo-600 hover:bg-indigo-100 rounded-lg px-2 py-2 transition-colors">
+          <Link to={`/users`} className={isActive('/users')}>
             <HiOutlineUsers size={20} />
             Users
           </Link>
         )}
-        <Link to={`/content`} className="flex items-center gap-3 hover:text-indigo-600 hover:bg-indigo-100 rounded-lg px-2 py-2 transition-colors">
+        <Link to={`/content`} className={isActive('/content')}>
           <HiOutlineDocumentText size={20} />
           Content
         </Link>
-        <Link to="/subscription" className="flex items-center gap-3 hover:text-indigo-600 hover:bg-indigo-100 rounded-lg px-2 py-2 transition-colors">
+        <Link to="/subscription" className={isActive('/subscription')}>
           <HiOutlineCreditCard size={20} />
           Subscription
         </Link>
